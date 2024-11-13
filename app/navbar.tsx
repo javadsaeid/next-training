c03c7524409d3d315778796edba1dbfe9b64e6f6
javad <javadsaeid8@gmail.com>
2024-11-13 21:32:26 +0330

    add google auth login

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='flex bg-slate-200 p-5'>
-      <Link href="/" className='mr-5'>Home</Link>
-      <Link href="/users" className='mr-5'>Users</Link>
-      <Link href="/admin" className='mr-5'>Admin</Link>
+        <Link href="/" className='mr-5'>Home</Link>
+        <Link href="/users" className='mr-5'>Users</Link>
+        <Link href="/admin" className='mr-5'>Admin</Link>
+        <Link href="/api/auth/signin" className='mr-5'>Login</Link>
     </div>
   )
 }
